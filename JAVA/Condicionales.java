7649f93cec629b989d6fa194d02a4572d1e8da2c
pavelio1303 <pablojimenez910.alu@stemgranada.com>
2024-01-09 09:45:28 +0100

    Condicionales número positivo o negativo poniendo a prueba la introducción de argumentos por consola

--- JAVA/Condicionales.java
+++ JAVA/Condicionales.java
@@ -4,9 +4,14 @@
 
 // La clase tiene que llamarse igual que el archivo, normalmente se crea solo una
 
+import java.util.Scanner;
+
 public class Condicionales {   // El nombre de la clase SIEMPRE va en mayúsculas !!!!!
 
     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
         int numero = 13;
 
         /* 
@@ -16,7 +21,7 @@ public class Condicionales {   // El nombre de la clase SIEMPRE va en mayúscula
             print("El número es igual a 5.")
         elif numero > 5:
             print("El número es mayor que 5.")
-        */
+       
         if (numero < 5){
             System.out.println("El número es menor que 5.");
         } else if (numero == 5){
@@ -24,5 +29,18 @@ public class Condicionales {   // El nombre de la clase SIEMPRE va en mayúscula
         } else {
             System.out.println("El número es mayor que 5.");
         }
+        */
+        if (args.length >= 1){
+            numero = Integer.parseInt(args[0]);
+        } else {
+            System.out.println("Introduce un número: ");
+            numero = sc.nextInt();
+        }
+
+        if (numero >= 0){
+                System.out.println(numero + " es positivo.");
+            } else {
+                System.out.println(numero + " es negativo.");
+            }
+        }
     } 
-}
